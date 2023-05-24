dced830016ec525f537818ebcbe02804775039ad
Bobo <bobo.kovacevic@gmail.com>
2023-05-24 15:34:49 +0200

    Assign random asset

--- rmrk_proxy/lib.rs
+++ rmrk_proxy/lib.rs
@@ -2,19 +2,28 @@
 
 #[ink::contract]
 mod rmrk_proxy {
-    use ink::env::{
-        call::{
-            build_call,
-            ExecutionInput,
-            Selector,
+    use crate::ensure;
+    use ink::{
+        env::{
+            call::{
+                build_call,
+                ExecutionInput,
+                Selector,
+            },
+            hash,
+            DefaultEnvironment,
         },
-        DefaultEnvironment,
+        prelude::vec::Vec,
     };
+    use openbrush::contracts::psp34::Id;
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
         MintingError,
+        OwnershipTransferError,
+        NoAssetsDefined,
+        TooManyAssets,
     }
 
     pub type Result<T> = core::result::Result<T, Error>;
@@ -23,6 +32,7 @@ mod rmrk_proxy {
     pub struct RmrkProxy {
         rmrk_contract: AccountId,
         catalog_contract: AccountId,
+        salt: u64,
     }
 
     impl RmrkProxy {
@@ -32,33 +42,50 @@ mod rmrk_proxy {
             Self {
                 rmrk_contract,
                 catalog_contract,
+                salt: 0,
             }
         }
 
         #[ink(message, payable)]
         pub fn mint(&mut self) -> Result<()> {
-            // let transferred_value = Self::env().transferred_value();
+            let transferred_value = Self::env().transferred_value();
             let caller = Self::env().caller();
 
-            let _mint_result = build_call::<DefaultEnvironment>()
+            let total_assets = build_call::<DefaultEnvironment>()
                 .call(self.rmrk_contract)
                 .gas_limit(5000000000)
                 .exec_input(ExecutionInput::new(Selector::new(ink::selector_bytes!(
-                    "Rmrk::mint"
+                    "MultiAsset::total_assets"
+                ))))
+                .returns::<u32>()
+                .try_invoke()
+                .unwrap();
+            ensure!(total_assets.unwrap() > 0, Error::NoAssetsDefined);
+            // This is temporary since current pseudo random generator is not working with big numbers.
+            ensure!(total_assets.unwrap() < 256, Error::TooManyAssets);
+
+            // TODO check why the call is failing silently when no transferred value is provided.
+            let _mint_result = build_call::<DefaultEnvironment>()
+                .call(self.rmrk_contract)
+                .gas_limit(5000000000)
+                .transferred_value(transferred_value)
+                .exec_input(ExecutionInput::new(Selector::new(ink::selector_bytes!(
+                    "MintingLazy::mint"
                 ))))
                 .returns::<()>()
-                .try_invoke();
-            ink::env::debug_println!("mint_result: {:?}", _mint_result);
+                .try_invoke()
+                .map_err(|_| Error::MintingError)?;
 
+            let asset_id = self.get_pseudo_random((total_assets.unwrap() -1)  as u8);
             let _add_asset_result = build_call::<DefaultEnvironment>()
                 .call(self.rmrk_contract)
                 .gas_limit(5000000000)
                 .exec_input(
                     ExecutionInput::new(Selector::new(ink::selector_bytes!(
-                        "Rmrk::add_asset_to_token"
+                        "MultiAsset::add_asset_to_token"
                     )))
-                    .push_arg(1)
-                    .push_arg(0),
+                    .push_arg(Id::U64(1))
+                    .push_arg(asset_id as u32),
                 )
                 .returns::<()>()
                 .try_invoke();
@@ -67,12 +94,14 @@ mod rmrk_proxy {
                 .call(self.rmrk_contract)
                 .gas_limit(5000000000)
                 .exec_input(
-                    ExecutionInput::new(Selector::new(ink::selector_bytes!("Rmrk::transfer_to")))
+                    ExecutionInput::new(Selector::new(ink::selector_bytes!("PSP34::transfer")))
                         .push_arg(caller)
-                        .push_arg(1),
+                        .push_arg(Id::U64(1))
+                        .push_arg(Vec::<u8>::new()),
                 )
                 .returns::<()>()
-                .try_invoke();
+                .try_invoke()
+                .map_err(|_| Error::OwnershipTransferError)?;
 
             Ok(())
         }
@@ -85,6 +114,19 @@ mod rmrk_proxy {
         #[ink(message)]
         pub fn get_catalog_contract_address(&self) -> AccountId {
             self.catalog_contract
+        }
+
+        /// Generates pseudo random number, Used to pick a random asset for a token. Big assumption is that we will have less than 256 assets.
+        fn get_pseudo_random(&mut self, max_value: u8) -> u8 {
+            let seed = self.env().block_timestamp();
+            let mut input: Vec<u8> = Vec::new();
+            input.extend_from_slice(&seed.to_be_bytes());
+            input.extend_from_slice(&self.salt.to_be_bytes());
+            let mut output = <hash::Keccak256 as hash::HashOutput>::Type::default();
+            ink::env::hash_bytes::<hash::Keccak256>(&input, &mut output);
+            self.salt += 1;
+            let number = output[0] % (max_value + 1);
+            number
         }
     }
 
@@ -102,9 +144,9 @@ mod rmrk_proxy {
             let rmrk: AccountId = [0x42; 32].into();
             let catalog: AccountId = [0x41; 32].into();
 
-            let cotract = RmrkProxy::new(rmrk, catalog);
-            assert_eq!(cotract.get_rmrk_contract_address(), rmrk);
-            assert_eq!(cotract.get_catalog_contract_address(), catalog);
+            let contract = RmrkProxy::new(rmrk, catalog);
+            assert_eq!(contract.get_rmrk_contract_address(), rmrk);
+            assert_eq!(contract.get_catalog_contract_address(), catalog);
         }
     }
 
@@ -117,19 +159,24 @@ mod rmrk_proxy {
     mod e2e_tests {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
-        use ink_e2e::build_message;
         use crate::rmrk_proxy::RmrkProxyRef;
         use catalog_example::catalog_example::CatalogContractRef;
-        use rmrk_equippable_lazy::rmrk_equippable_lazy::RmrkRef;
+        use ink::primitives::AccountId;
+        use ink_e2e::build_message;
+        use openbrush::contracts::psp34::{
+            psp34_external::PSP34,
+            Id,
+        };
         use rmrk::{
             storage::catalog_external::Catalog,
+            traits::multiasset_external::MultiAsset,
             types::{
                 Part,
                 PartType,
             },
         };
-        use openbrush::contracts::psp34::psp34_external::PSP34;
-        
+        use rmrk_equippable_lazy::rmrk_equippable_lazy::RmrkRef;
+
         type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
         #[ink_e2e::test]
@@ -184,7 +231,22 @@ mod rmrk_proxy {
                 .await
                 .expect("RMRK contract instantiation failed")
                 .account_id;
-            // ink::env::debug_println!("rmrk_address: {:?}", rmrk_address);
+
+            // Add asset to RMRK contract.
+            let add_asset_entry_message =
+                build_message::<RmrkRef>(rmrk_address.clone()).call(|rmrk| {
+                    rmrk.add_asset_entry(
+                        Some(catalog_contract_address.clone()),
+                        0,
+                        1,
+                        String::from("ipfs://parturi").into(),
+                        vec![0],
+                    )
+                });
+            client
+                .call(&alice, add_asset_entry_message, 0, None)
+                .await
+                .expect("Add asset entry failed");
 
             // Proxy contract
             let proxy_constructor = RmrkProxyRef::new(rmrk_address, catalog_contract_address);
@@ -198,7 +260,7 @@ mod rmrk_proxy {
             let mint_message =
                 build_message::<RmrkProxyRef>(proxy_address.clone()).call(|proxy| proxy.mint());
             client
-                .call(&ink_e2e::alice(), mint_message, 0, None)
+                .call(&alice, mint_message, 1_000_000_000_000_000_000, None)
                 .await
                 .expect("Mint failed");
 
@@ -207,12 +269,50 @@ mod rmrk_proxy {
                 build_message::<RmrkRef>(rmrk_address.clone()).call(|rmrk| rmrk.total_supply());
 
             let read_total_supply_result = client
-                .call_dry_run(&ink_e2e::alice(), &read_total_supply_message, 0, None)
+                .call_dry_run(&alice, &read_total_supply_message, 0, None)
                 .await
                 .return_value();
             assert_eq!(read_total_supply_result, 1);
 
+            // Check if asset has been added to token
+            let read_total_assets_message =
+                build_message::<RmrkRef>(rmrk_address.clone()).call(|rmrk| rmrk.total_assets());
+
+            let read_total_assets_result = client
+                .call_dry_run(&alice, &read_total_assets_message, 0, None)
+                .await
+                .return_value();
+            assert_eq!(read_total_assets_result, 1);
+
+            // Check if token owner is correct
+            let read_owner_of_message = build_message::<RmrkRef>(rmrk_address.clone())
+                .call(|rmrk| rmrk.owner_of(Id::U64(1)));
+
+            let read_owner_of_result = client
+                .call_dry_run(&alice, &read_owner_of_message, 0, None)
+                .await
+                .return_value()
+                .unwrap();
+            ink::env::debug_println!(
+                "token_owner: {:?} {:?}",
+                read_owner_of_result,
+                *alice.account_id()
+            );
+            // assert_eq!(read_owner_of_result, *alice.account_id().);
+
             Ok(())
         }
     }
+}
+
+/// Evaluate `$x:expr` and if not true return `Err($y:expr)`.
+///
+/// Used as `ensure!(expression_to_ensure, expression_to_return_on_false)`.
+#[macro_export]
+macro_rules! ensure {
+    ( $x:expr, $y:expr $(,)? ) => {{
+        if !$x {
+            return Err($y.into())
+        }
+    }};
 }
